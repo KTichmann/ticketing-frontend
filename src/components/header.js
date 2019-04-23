1b1b221f16198dd0405c8b4c76da48d51cd4ad90
ktichmann <ktichmann@gmail.com>
2019-04-23 21:41:32 +0200

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,76 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Drawer from "@material-ui/core/Drawer"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import CloseIcon from "@material-ui/icons/Close"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import DashboardIcon from "@material-ui/icons/Dashboard"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  state = {
+    open: false,
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleDrawerClose = () => {
+    this.setState({ open: false })
+  }
+
+  render() {
+    const { open } = this.state
+
+    return (
+      <div>
+        <AppBar position="fixed">
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="persistent" anchor="left" open={open}>
+          <div style={{ marginLeft: "9rem" }}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <ListItem button key={"Admin Dashboard"}>
+              <ListItemIcon style={{ marginRight: "0px" }}>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Admin Dashboard"} />
+            </ListItem>
+            {/* {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))} */}
+          </List>
+        </Drawer>
+      </div>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
